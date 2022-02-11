5d9ed67e5e5911e4acdbf8a72007289dc83a3717
Ary EJFA <36819538+aryejfa@users.noreply.github.com>
2022-02-11 18:42:17 +0700

    Update header.php

--- header.php
+++ header.php
@@ -12,7 +12,6 @@
       <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
         <span class="sr-only">Toggle navigation</span>
       </a>
-
       <div class="navbar-custom-menu">
         <ul class="nav navbar-nav">
           <!-- Messages: style can be found in dropdown.less-->
